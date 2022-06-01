570f50c6a5f6ce1ab1fe4ca7fcaea8c117c57c20
Francois Daoust <fd@tidoust.net>
2022-06-01 22:47:30 +0200

    Add script to create mappings PR per data file
    The `create-mappings-pr.js` script proposes new mappings in separate pull
    requests, one per data file. The description of the pull request details the
    actual changes, and suggests additional things to review (notably the need to
    look at `representative` flags.
    
    The script creates up to 5 pull requests by default.
    
    A GitHub token needs to be available as a `GH_TOKEN` environment variable or
    within a `config.json` file.

---
+++ src/octokit.js
@@ -0,0 +1,39 @@
+/**
+ * Wrapper around Octokit to add throttling and avoid hitting rate limits
+ */
+
+import { throttling } from '@octokit/plugin-throttling';
+import { Octokit as rawOctokit } from '@octokit/rest';
+
+const throttledOctokit = rawOctokit.plugin(throttling);
+
+const MAX_RETRIES = 3;
+
+export function Octokit(params) {
+  params = params || {};
+
+  const octoParams = Object.assign({
+    throttle: {
+      onRateLimit: (retryAfter, options) => {
+        if (options.request.retryCount < MAX_RETRIES) {
+          console.warn(`Rate limit exceeded, retrying after ${retryAfter} seconds`)
+          return true;
+        } else {
+          console.error(`Rate limit exceeded, giving up after ${MAX_RETRIES} retries`);
+          return false;
+        }
+      },
+      onSecondaryRateLimit: (retryAfter, options) => {
+        if (options.request.retryCount < MAX_RETRIES) {
+          console.warn(`Abuse detection triggered, retrying after ${retryAfter} seconds`)
+          return true;
+        } else {
+          console.error(`Abuse detection triggered, giving up after ${MAX_RETRIES} retries`);
+          return false;
+        }
+      }
+    }
+  }, params);
+
+  return new throttledOctokit(octoParams);
+}
